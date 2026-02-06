4d3e9baa94602a4dfb9ae59b41e828144fe7d5f8
Gepardgame <75749982+Gepardgame@users.noreply.github.com>
2026-02-06 15:54:52 +0100

    strt inv

---
+++ src/inv_ntt_node.vhdl
@@ -0,0 +1,115 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+library work;
+  use work.globals.all;
+  use work.zeta_lut.all;
+
+entity inv_ntt_node is
+  generic (
+    zeta_expo : natural;
+    depth     : natural;
+    size      : natural
+  );
+  port (
+    clock   : in    std_logic;
+    counter : in    natural;
+    a       : in    natural_polynomial(size - 1 downto 0);
+    ntt_a   : out   natural_polynomial(size - 1 downto 0)
+  );
+end entity inv_ntt_node;
+
+architecture a_inv_ntt_node of ntt_node is
+
+  constant zeta_pow : modq_t := zetas(zeta_expo);
+
+  signal proc_a : natural_polynomial(size - 1 downto 0);
+
+  component inv_ntt_node is
+    generic (
+      zeta_expo : natural;
+      depth     : natural;
+      size      : natural
+    );
+    port (
+      clock   : in    std_logic;
+      counter : in    natural;
+      a       : in    natural_polynomial(size - 1 downto 0);
+      ntt_a   : out   natural_polynomial(size - 1 downto 0)
+    );
+  end component inv_ntt_node;
+
+  function mod_add (
+    a,
+    b : signed
+  ) return signed is
+  begin
+
+    return resize((a + b) mod q, q_len);
+
+  end function mod_add;
+
+  function mod_sub (
+    a,
+    b : signed
+  ) return signed is
+  begin
+
+    return resize((a - b) mod q, q_len);
+
+  end function mod_sub;
+
+begin
+
+  p_ntt_step : process (clock) is
+  begin
+
+    if rising_edge(clock) then
+      if (counter = depth) then
+        proc_a <= a;
+      end if;
+    end if;
+
+  end process p_ntt_step;
+
+  normal_node : if (size > 1) generate
+    signal rigth_result : natural_polynomial((size / 2) - 1 downto 0);
+    signal left_result  : natural_polynomial((size / 2) - 1 downto 0);
+    signal rigth_a      : natural_polynomial((size / 2) - 1 downto 0);
+    signal left_a       : natural_polynomial((size / 2) - 1 downto 0);
+  begin
+
+    left_node : component inv_ntt_node
+      generic map (
+        zeta_expo => zeta_expo / 2, depth => depth - 1, size => size / 2
+      )
+      port map (
+        clock   => clock,
+        counter => counter,
+        a       => proc_a(size / 2 - 1 downto 0),
+        ntt_a   => left_result
+      );
+
+    right_node : component inv_ntt_node
+      generic map (
+        zeta_expo => zeta_expo / 2 + n / 2, depth => depth - 1, size => size / 2
+      )
+      port map (
+        clock   => clock,
+        counter => counter,
+        a       => proc_a(size - 1 downto size / 2),
+        ntt_a   => rigth_result
+      );
+
+    rigth_a <= (left_result & rigth_result);
+    left_a  <= (left_result & rigth_result);
+    ntt_a   <= left_a & rigth_a;
+
+  end generate normal_node;
+
+  leaf_node : if (size = 1) generate
+    ntt_a <= proc_a;
+  end generate leaf_node;
+
+end architecture a_inv_ntt_node;
---
+++ src/inv_ntt_root.vhdl
@@ -0,0 +1,93 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+library work;
+  use work.globals.all;
+
+entity inv_ntt_root is
+  port (
+    clock    : in    std_logic;
+    a        : in    polynomial;
+    ntt_a    : out   polynomial;
+    finished : out   std_logic
+  );
+end entity inv_ntt_root;
+
+architecture a_inv_ntt_root of ntt_root is
+
+  component inv_ntt_node is
+    generic (
+      zeta_expo : natural;
+      depth     : natural;
+      size      : natural
+    );
+    port (
+      clock   : in    std_logic;
+      counter : in    natural;
+      a       : in    natural_polynomial(size - 1 downto 0);
+      ntt_a   : out   natural_polynomial(size - 1 downto 0)
+    );
+  end component inv_ntt_node;
+
+  component countdown_clock is
+    generic (
+      -- count limit of the timer
+      counter_limit : natural := 8
+    );
+    port (
+      -- clock, counter enabled and reset
+      slv_clock           : in    std_logic;
+      slv_counter_enabled : in    std_logic;
+      slv_reset           : in    std_logic;
+      slv_write           : in    std_logic;
+      -- output 1 for one cycle when timer is complete
+      slv_output : out   std_logic;
+      -- internal counter interfaces
+      slv_counter_in  : in    natural;
+      slv_counter_out : out   natural
+    );
+  end component countdown_clock;
+
+  signal proc_a  : polynomial;
+  signal counter : natural;
+
+begin
+
+  count : component countdown_clock
+    generic map (
+      counter_limit => ntt_tree_depth
+    )
+    port map (
+      slv_clock           => clock,
+      slv_counter_enabled => '1',
+      slv_reset           => '0',
+      slv_write           => '0',
+      slv_output          => finished,
+      slv_counter_in      => 0,
+      slv_counter_out     => counter
+    );
+
+  p_ntt : process (clock) is
+  begin
+
+    if rising_edge(clock) then
+      proc_a <= a;
+    end if;
+
+  end process p_ntt;
+
+  ntt : component inv_ntt_node
+    generic map (
+      zeta_expo => n,
+      depth     => ntt_tree_depth,
+      size      => n
+    )
+    port map (
+      clock   => clock,
+      counter => counter,
+      a       => proc_a,
+      ntt_a   => ntt_a
+    );
+
+end architecture a_inv_ntt_root;
